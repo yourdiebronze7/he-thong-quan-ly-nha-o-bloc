f6f09a24cb1e49fd2a47d86747e23eb05728edd9
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-07 12:25:43 -0700

    Updated the Infura URL to use the v3 endpoint format.

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ const Web3 = require('web3');
 
 const provider = new HDWalletProvider(
     'YOUR_MNEMONIC',
-    'https://testnet.infura.io/v3/YOUR_INFURA_PROJECT_ID'
+    'https://rinkeby.infura.io/v3/YOUR_INFURA_PROJECT_ID'
 );
 const web3 = new Web3(provider);
 
