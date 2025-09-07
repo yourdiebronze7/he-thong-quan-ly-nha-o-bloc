a56f71565b9342e42d47b168d36ec7d91f895885
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-07 12:25:58 -0700

    Update Rinkeby network configuration to use Alchemy instead of Infura

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ const Web3 = require('web3');
 
 const provider = new HDWalletProvider(
     'YOUR_MNEMONIC',
-    'https://rinkeby.infura.io/v3/YOUR_INFURA_PROJECT_ID'
+    'https://eth-rinkeby.alchemyapi.io/v2/YOUR_ALCHEMY_API_KEY'
 );
 const web3 = new Web3(provider);
 
